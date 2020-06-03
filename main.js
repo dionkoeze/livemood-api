8dc2e9ca1fe6ac02aec3940c10bab953e8d453ae
Dion Koeze <dion.koeze@gmail.com>
2020-06-03 23:33:03 +0200

    Get cookie at client

--- main.js
+++ main.js
@@ -2,14 +2,28 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const jwt = require('jsonwebtoken');
 
 server.listen(process.env.PORT || 3000);
 
-app.use(express.static('public'));
+app.post('/auth', (req, res) => {
+  const token = jwt.sign(
+    {
+    id: 1234567890,
+  }, 
+  process.env.SECRET || 'superSecret', 
+  {
+    expiresIn: 10*60 * 60,
+  });
+
+  res.status(200).cookie('identity', token, {httpOnly: true, secure: true}).send("You're authenticated!");
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + 'public/index.html');
 });
+
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
   socket.emit('hello', 'hi there!');
--- public/index.js
+++ public/index.js
@@ -1,1 +1,6 @@
 console.log('yo');
+
+$.post("/auth", function(data) {
+    console.log(data);
+    $('#response').text(data);
+});
